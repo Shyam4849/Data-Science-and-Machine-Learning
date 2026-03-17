2697de286132864571b54f65ac8254e0743c7d7f
Shyam4849 <piyush.soni8877@gmail.com>
2026-03-17 19:39:11 +0530

    Logging Real World Example

---
+++ 13-LoggingInPython.ipynb/app.py
@@ -0,0 +1,46 @@
+import logging
+
+# Logging setting
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s-%(name)s-%(levelname)s-%(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.FileHandler("app1.log"), logging.StreamHandler()],
+)
+
+logger = logging.getLogger("ArithmeticAPP")
+
+
+def add(a, b):
+    result = a + b
+    logger.debug(f"Adding {a} + {b} = {result}")
+    return result
+
+
+def subtract(a, b):
+    result = a - b
+    logger.debug(f"Sutracting {a} - {b} = {result}")
+    return result
+
+
+def multiply(a, b):
+    result = a * b
+    logger.debug(f"Multiplying {a} * {b} = {result}")
+    return result
+
+
+def divide(a, b):
+    try:
+        result = a / b
+        logger.debug(f"Dividing {a} + {b} = {result}")
+        return result
+    except ZeroDivisionError:
+        logger.error("Division by zero error")
+        return None
+
+
+add(10, 15)
+subtract(15, 10)
+multiply(10, 30)
+divide(30, 15)
