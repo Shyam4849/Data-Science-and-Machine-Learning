c71af056bd18554d038e8e153b6684af619e1d21
Shyam4849 <piyush.soni8877@gmail.com>
2026-09-23 00:30:16 +0530

    Logging and Exception

--- 45-End-to-End-Machine-Learning-Projects-with-AWS-Azure-Implementation/mlproject/src/logger.py
+++ 45-End-to-End-Machine-Learning-Projects-with-AWS-Azure-Implementation/mlproject/src/logger.py
@@ -2,17 +2,17 @@ import logging
 import os
 from datetime import datetime
 
-# 1. Timestamp aur clean file name configure karein
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-# 2. 'src/logs' ke andar timestamp folder ka path set karein
-SRC_DIR = os.path.dirname(os.path.abspath(__file__))
-logs_path = os.path.join(SRC_DIR, "logs", LOG_FILE.replace(".log", ""))
 
-# CORRECTION: Sirf folder banayein, file name ke saath nahi
+SRC_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(SRC_DIR)
+
+
+logs_path = os.path.join(ROOT_DIR, "logs", LOG_FILE.replace(".log", ""))
 os.makedirs(logs_path, exist_ok=True)
 
-# 3. Final single log file path
+
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
